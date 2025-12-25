1599f02d0e7c5eaa6def4fdb95f2598316e70e90
Do Ngoc Cuong <cuong01656499081@gmail.com>
2025-12-25 14:31:35 +0700

    getAllImage successfully

--- backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/Repository/ImageRepository.java
+++ backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/Repository/ImageRepository.java
@@ -4,5 +4,8 @@ package com.smart_restaurant.demo.Repository;
 import com.smart_restaurant.demo.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image,Integer> {
+    List<Image> findAllByItem_ItemId(Integer itemId);
 }
--- backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/Service/ImageService.java
+++ backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/Service/ImageService.java
@@ -2,10 +2,12 @@ package com.smart_restaurant.demo.Service;
 
 import com.smart_restaurant.demo.dto.Request.ImageRequest;
 import com.smart_restaurant.demo.dto.Response.ImageResponse;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.util.List;
 
 public interface ImageService {
     List<ImageResponse> uploadImage(Integer itemId, ImageRequest imageRequest);
     String deleteImage(Integer id);
+    List<ImageResponse> getAllImage(Integer itemId,JwtAuthenticationToken jwtAuthenticationToken);
 }
