f4e67dfed9dd0fe825f9fb835fa91e719460652b
Do Ngoc Cuong <cuong01656499081@gmail.com>
2026-01-05 21:08:24 +0700

    feature/cuong/create-pdf-invoice successfully

--- backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/Service/OrderService.java
+++ backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/Service/OrderService.java
@@ -4,6 +4,7 @@ import com.smart_restaurant.demo.dto.Response.InvoiceResponse;
 
 import com.smart_restaurant.demo.dto.Request.OrderRequest;
 import com.smart_restaurant.demo.dto.Response.OrderResponse;
+import com.smart_restaurant.demo.entity.Order;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.util.List;
@@ -14,4 +15,5 @@ public interface OrderService {
     OrderResponse createOrder(OrderRequest orderRequest, JwtAuthenticationToken jwtAuthenticationToken);
     List<OrderResponse> getAllOrder(JwtAuthenticationToken jwtAuthenticationToken);
     List<OrderResponse> getAllTenantOrder(JwtAuthenticationToken jwtAuthenticationToken);
+    byte[] generateInvoicePdf(Integer orderId,JwtAuthenticationToken jwtAuthenticationToken);
 }
